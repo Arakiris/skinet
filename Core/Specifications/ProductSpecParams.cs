b61a9eca75a80cf42a81221c70287a4c45061f25
Arakiris <f.vongvilay@gmail.com>
2022-07-31 16:45:52 +0200

    End of section 11

--- Core/Specifications/ProductSpecParams.cs
+++ Core/Specifications/ProductSpecParams.cs
@@ -12,7 +12,7 @@ namespace Core.Specifications
         }
         public int? BrandId { get; set; }
         public int? TypeId { get; set; }
-        public string? Sort { get; set; }
+        public string Sort { get; set; }
         private string _search;
         public string Search {
             get => _search;
