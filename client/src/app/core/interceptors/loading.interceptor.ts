efa6acc1d9572d6c8cbec81169484451e2524463
Arakiris <f.vongvilay@gmail.com>
2022-12-04 15:27:13 +0100

    End of section 22

--- client/src/app/core/interceptors/loading.interceptor.ts
+++ client/src/app/core/interceptors/loading.interceptor.ts
@@ -13,7 +13,10 @@ export class LoadingInterceptor implements HttpInterceptor {
         if(req.method == 'POST' && req.url.includes('orders')) {
             return next.handle(req);
         }
-        if (!req.url.includes('emailexists')){
+        if(req.method === 'DELETE') {
+            return next.handle(req);
+        }
+        if (req.url.includes('emailexists')){
             return next.handle(req);
         }
         this.busyService.busy();
